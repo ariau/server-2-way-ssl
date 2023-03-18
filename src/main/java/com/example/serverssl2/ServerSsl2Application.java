578cc7ef7a2e838b5e67205124b4c455ada68165
ariau <alexisdeclairebois@gmail.com>
2023-03-18 19:31:06 -0400

    Initial commit

---
+++ src/main/java/com/example/serverssl2/ServerSsl2Application.java
@@ -0,0 +1,13 @@
+package com.example.serverssl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSsl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSsl2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serverssl2/ServerSsl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverssl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSsl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
